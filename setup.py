d21eefe88a2c3e75c1267941e1e22fee378c8cac
Ben LaRoque <laroque@physics.ucsb.edu>
2016-05-02 11:02:50 -0700

    a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name='p150s16',
+    packages=['Adafruit'],
+    install_requires=['RPi.GPIO'],
+    url='http://www.github.com/laroque/p150s16',
+)
